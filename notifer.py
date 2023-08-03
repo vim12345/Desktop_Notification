34a40ca0838309d20d61961a03b0ee8fb2dc0b60
vim12345 <vk2699945@gmail.com>
2023-08-03 20:30:08 +0530

    first commit

---
+++ notifer.py
@@ -0,0 +1,15 @@
+from plyer import notification
+
+notification.notify(
+    title = "Hello Vimal Sir",
+    message = "Vimal Sir Notification Aaya hai",
+    app_icon = None,
+    timeout = 10,
+)
+
+notification.notify(
+    title = "Hello Vimal Sir",
+    message = "Vimal Sir Notification Aaya hai",
+    app_icon = None,
+    timeout = 10,
+)
